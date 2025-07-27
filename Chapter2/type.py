61f7555915529e8a6c053d970b84bf0e358aac35
Amanpatidar8085 <patidaraman444@gmail.com>
2025-07-27 16:25:54 +0530

    add file

---
+++ Chapter2/type.py
@@ -0,0 +1,22 @@
+#   int type
+a=3
+t=type(a)
+print(t)
+ #   String type
+a="Aman"
+t=type(a)
+print(t)
+
+#   float type
+a=3.634
+t=type(a)
+print(t)
+
+#         type conversion
+a="31"
+b=int(a)
+print(b)
+
+c="31.4456768"
+d=float(c)
+print(d)
